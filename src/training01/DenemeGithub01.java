1330234df509513657750617d1c48394f370eafc
ayse <atrk61@gmail.com>
2021-02-28 22:57:53 +0100

    asyem branch commit

--- src/training01/DenemeGithub01.java
+++ src/training01/DenemeGithub01.java
@@ -5,6 +5,11 @@ public class DenemeGithub01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Merhahaba Github ....");
+		
+		System.out.println(" asyem branchi inn commiti ");
+		
 	}
 
 }
+ 
