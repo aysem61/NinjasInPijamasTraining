5913773e99338281eb2add67753345827befa49f
ayse <atrk61@gmail.com>
2021-03-01 11:04:30 +0100

    ilave satirdan sonra

--- src/training01/DenemeGithub01.java
+++ src/training01/DenemeGithub01.java
@@ -5,6 +5,8 @@ public class DenemeGithub01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Local master icin ilk commit asyem gecmeden once ");
+		
+		System.out.println("ben ayse can merhabalar herkese ");
 
 	}
 
