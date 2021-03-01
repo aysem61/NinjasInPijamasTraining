bbde0ecdca4ee84abfd4e8b35604d763ae53f5cf
ayse <atrk61@gmail.com>
2021-03-01 11:58:33 +0100

    ucuncu commit

---
+++ src/training02/Git01.java
@@ -0,0 +1,16 @@
+package training02;
+
+public class Git01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Hello all 
+		System.out.println(2+4);
+		
+		System.out.println("Git ucuncu commit icin birseyler ....");
+		
+		
+		}
+
+}
